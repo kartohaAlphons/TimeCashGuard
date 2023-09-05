2d4ff481e4ff4246b8b265c807769146f243623c
Maksim Komar <mr.bomber1993@gmail.com>
2023-09-05 13:54:17 +0300

    initial commit

---
+++ TimeCashGuard/MainScreen.swift
@@ -0,0 +1,18 @@
+//
+//  MainScreen.swift
+//  TimeCashGuard
+//
+//  Created by Максим Комар on 4.09.23.
+//
+
+ import UIKit
+
+class MainScreen: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "MAIN"
+        view.backgroundColor = .white
+        
+    }
+}
